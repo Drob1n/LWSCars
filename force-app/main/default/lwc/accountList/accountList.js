b03a8c8773b83e434f324084f59c89ec312f5ab2
Nikoletta <nicole.snisar@gmail.com>
2022-08-29 10:59:46 +0200

    Project update

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,19 +1,9 @@
 import { LightningElement, wire} from 'lwc';
-import getAllObject1 from '@salesforce/apex/AccountController.getAllObject1';
-import getAllObject2 from '@salesforce/apex/AccountController.getAllObject2';
-import getAllObject3 from '@salesforce/apex/AccountController.getAllObject3';
-import getAllObject4 from '@salesforce/apex/AccountController.getAllObject4';
+import getAllAccounts from '@salesforce/apex/AccountController.getAllAccounts';
 
 export default class AccountList extends LightningElement {
-    @wire(getAllObject1)
+    @wire(getAllAccounts)
     accounts;
 
-    @wire(getAllObject2)
-    accounts;
 
-    @wire(getAllObject3)
-    accounts;
-
-    @wire(getAllObject4)
-    accounts;
 }
