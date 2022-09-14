d41843e7f2611d3079640938baa385abaec0b60e
Nikoletta <nicole.snisar@gmail.com>
2022-09-14 10:54:33 +0200

    14.09

--- force-app/main/default/lwc/showroomsList/showroomsList.js
+++ force-app/main/default/lwc/showroomsList/showroomsList.js
@@ -1,20 +1,17 @@
-import { LightningElement, wire, api } from 'lwc';
+import { LightningElement, wire,  api } from 'lwc';
 import getAllRelatedShowrooms from '@salesforce/apex/ShowroomController.getAllRelatedShowrooms';
 export default class ShowroomsList extends LightningElement {
-	 @api recordId;
-	
-	@wire (getAllRelatedShowrooms, { accountId: '$recordId'})
-	showrooms;
-// 	// .then((data) => { 
-// 		if(data){
-// 		for(let key in data) {
-// 		 this.mapData.push({value: data[key], key:key, url : 
-// 		'https://wise-moose-v2ezd-dev-ed.lightning.force.com/lightning/r/Showroom__c/' 
-// 		+ key + '/view' });
-// 	}      
-// }
-}
-    // showrooms;
+	@wire(getAllRelatedShowrooms)
+    showrooms;
+    @api ShowroomsList = getAllRelatedShowrooms;
+
+    // showrooms = getAllRelatedShowrooms;
+
+    // handleEvent(){
+    //     this.showrooms;
+    // }
+		
+	}
 
 
 	
