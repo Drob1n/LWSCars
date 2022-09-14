620b7748e6e77b3c25578211d9dd97c4646bf519
Nikoletta <nicole.snisar@gmail.com>
2022-09-14 10:53:51 +0200

    14.09

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,9 +1,22 @@
 import { LightningElement, wire} from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountController.getAllAccounts';
+import getAllRelatedShowrooms from '@salesforce/apex/ShowroomController.getAllRelatedShowrooms';
 
 export default class AccountList extends LightningElement {
     @wire(getAllAccounts)
     accounts;
 
-
+getAllRelatedShowrooms;
+handleGetShowrooms(){
+this.template.getAllRelatedShowrooms ('c-c/showroomsList').getAllRelatedShowrooms();
 }
+}
+
+
+
+
+
+// accounts.addEventListener('account.Name', myFunction);
+// function myFunction(){
+//     getAllRelatedShowrooms;
+// }
