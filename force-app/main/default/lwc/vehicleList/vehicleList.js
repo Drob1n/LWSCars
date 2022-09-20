89dbc00218e9054e786959905bcf20356bc7f89b
Nikoletta <nicole.snisar@gmail.com>
2022-09-20 10:54:33 +0200

    20.09 New update

--- force-app/main/default/lwc/vehicleList/vehicleList.js
+++ force-app/main/default/lwc/vehicleList/vehicleList.js
@@ -1,13 +1,15 @@
-import { LightningElement, api } from 'lwc';
-import NAME_FIELD from '@salesforce/schema/Vehicle__c.Name';
-import VEH_MODEL_FIELD from '@salesforce/schema/Vehicle__c.Model__c';
-import VEH_PRICE_FIELD from '@salesforce/schema/Vehicle__c.Price__c';
-import VEH_PHOTO_FIELD from '@salesforce/schema/Vehicle__c.Photo__c';
+import { LightningElement, wire, track } from 'lwc';
+import getAllRelatedVehicles from '@salesforce/apex/AccountController.getAllRelatedVehicles'
 export default class VehicleCardList extends LightningElement {
-    @api vehicle = {
-        "image" : VEH_PHOTO_FIELD,
-        "name" : NAME_FIELD,
-        "model" : VEH_MODEL_FIELD,
-        "price" : VEH_PRICE_FIELD
-    };
+
+    @track selectedVehicles;
+
+    @wire(getAllRelatedVehicles, {showroomId: '$showroomOfferId'})
+    vehicles;
+
+    onVehicleSelection(event) {
+        console.log(event.target.value)
+        // this.selectedVehicles = event.detail.selectedRows;
+        this.showroomId = event.target.value;
+    }
 }
