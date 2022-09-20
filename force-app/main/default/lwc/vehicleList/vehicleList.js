f5f1772bb5e53d2ce6cbd058b3a2d1a868fefd4a
Nikoletta <nicole.snisar@gmail.com>
2022-09-20 09:49:50 +0200

    UPDATE

--- force-app/main/default/lwc/vehicleList/vehicleList.js
+++ force-app/main/default/lwc/vehicleList/vehicleList.js
@@ -1,7 +1,13 @@
-import { LightningElement, wire, api } from 'lwc';
-import getAllRelatedVehicles from '@salesforce/apex/VehicleController.getAllRelatedVehicles';
-export default class VehicleList extends LightningElement {
-	 @api recordId;
-	
-	@wire (getAllRelatedVehicles, { showroomId: '$recordId'})
-    vehicles;}
+import { LightningElement, api } from 'lwc';
+import NAME_FIELD from '@salesforce/schema/Vehicle__c.Name';
+import VEH_MODEL_FIELD from '@salesforce/schema/Vehicle__c.Model__c';
+import VEH_PRICE_FIELD from '@salesforce/schema/Vehicle__c.Price__c';
+import VEH_PHOTO_FIELD from '@salesforce/schema/Vehicle__c.Photo__c';
+export default class VehicleCardList extends LightningElement {
+    @api vehicle = {
+        "image" : VEH_PHOTO_FIELD,
+        "name" : NAME_FIELD,
+        "model" : VEH_MODEL_FIELD,
+        "price" : VEH_PRICE_FIELD
+    };
+}
