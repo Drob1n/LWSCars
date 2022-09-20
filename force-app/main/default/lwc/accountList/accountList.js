261bb2c835e80cbd8f9af3ca106963583e8c7b73
Nikoletta <nicole.snisar@gmail.com>
2022-09-20 09:50:08 +0200

    20.09 Update

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,41 +1,34 @@
-import { LightningElement} from 'lwc';
+import { LightningElement, wire, track} from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountController.getAllAccounts';
-
-
+import getRelatedShowrooms from '@salesforce/apex/AccountController.getRelatedShowrooms';
+import getAllRelatedVehicles from '@salesforce/apex/AccountController.getAllRelatedVehicles';
+const COLUMNS = [{label: 'Name', fieldName: 'Name'}];
 export default class AccountList extends LightningElement {
-    accounts;
-    error;
-    connectedCallback() {
-		this.loadAccounts();
-	}
-	loadAccounts() {
-		getAllAccounts()
-			.then(result => {
-				this.accounts = result;
-			})
-			.catch(error => {
-				this.error = error;
-			});
-    }
+        @track accountId = '';
+        @track showroomId;
+        //@track showrooms;
+        //@track vehicles;
+        @track columns = COLUMNS;
+        @track selectedVehicles;
+        @wire(getAllAccounts)
+        accounts;
+        @wire(getRelatedShowrooms, {accountId: '$accountId'})
+        showrooms;
+        @wire(getAllRelatedVehicles, {showroomId: '$showroomOfferId'})
+        vehicles;
+        onValueSelection(event) {
+            console.log(this.showrooms)
+            this.accountId = event.target.value;
+        }
+        onValueSelection2(event) {
+            this.showroomId = event.target.value;
+        }
 
-	showShowrooms(event){
-		this.isLoading = true;
-		let id = event.currentTarget.dataset.id;
-
-		getShowrooms({outAccount :id})
-		.then(result => {
-			{this.isShowroomSelected = false};
-
-			if(result.lenght >0){
-				{this.isAccountSelected = true};
-				{this.chosenShowrooms = result};
-				{this.isLoading = false};
-			}
-		})
-	}
-
-
-
+        onVehicleSelection(event) {
+            console.log(event.target.value)
+            // this.selectedVehicles = event.detail.selectedRows;
+            this.showroomId = event.target.value;
+        }
 }
 
 
