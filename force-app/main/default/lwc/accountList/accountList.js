26172b1c7eb4806b921443362e14e8e99bcaed16
Nikoletta <nicole.snisar@gmail.com>
2022-09-20 10:54:17 +0200

    Changes

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,32 +1,23 @@
 import { LightningElement, wire, track} from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountController.getAllAccounts';
 import getRelatedShowrooms from '@salesforce/apex/AccountController.getRelatedShowrooms';
-import getAllRelatedVehicles from '@salesforce/apex/AccountController.getAllRelatedVehicles';
 const COLUMNS = [{label: 'Name', fieldName: 'Name'}];
 export default class AccountList extends LightningElement {
         @track accountId = '';
         @track showroomId;
-        //@track showrooms;
-        //@track vehicles;
         @track columns = COLUMNS;
-        @track selectedVehicles;
+
         @wire(getAllAccounts)
         accounts;
+
         @wire(getRelatedShowrooms, {accountId: '$accountId'})
         showrooms;
-        @wire(getAllRelatedVehicles, {showroomId: '$showroomOfferId'})
-        vehicles;
+
         onValueSelection(event) {
             console.log(this.showrooms)
             this.accountId = event.target.value;
         }
         onValueSelection2(event) {
-            this.showroomId = event.target.value;
-        }
-
-        onVehicleSelection(event) {
-            console.log(event.target.value)
-            // this.selectedVehicles = event.detail.selectedRows;
             this.showroomId = event.target.value;
         }
 }
