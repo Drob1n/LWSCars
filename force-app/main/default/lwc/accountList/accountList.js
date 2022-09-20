a5a127ecc76b44745f03388cfd96a20eb37ca854
Nikoletta <nicole.snisar@gmail.com>
2022-09-20 12:42:01 +0200

    Last update

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,11 +1,13 @@
 import { LightningElement, wire, track} from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountController.getAllAccounts';
 import getRelatedShowrooms from '@salesforce/apex/AccountController.getRelatedShowrooms';
+import getAllRelatedVehicles from '@salesforce/apex/VehicleController.getAllRelatedVehicles'
 const COLUMNS = [{label: 'Name', fieldName: 'Name'}];
 export default class AccountList extends LightningElement {
         @track accountId = '';
         @track showroomId;
         @track columns = COLUMNS;
+        @track vehicles;
 
         @wire(getAllAccounts)
         accounts;
@@ -13,13 +15,27 @@ export default class AccountList extends LightningElement {
         @wire(getRelatedShowrooms, {accountId: '$accountId'})
         showrooms;
 
+
         onValueSelection(event) {
-            console.log(this.showrooms)
+            
             this.accountId = event.target.value;
+            console.log(this.showrooms)
         }
         onValueSelection2(event) {
+
             this.showroomId = event.target.value;
+
+            getAllRelatedVehicles({showroomId: this.showroomId})
+
+            .then(result=>{this.vehicles=result;
+
+                console.log(this.vehicles);})
+
         }
+
+
+
+
 }
 
 
--- force-app/main/default/lwc/vehicleList/vehicleList.js
+++ force-app/main/default/lwc/vehicleList/vehicleList.js
@@ -1,15 +1,5 @@
-import { LightningElement, wire, track } from 'lwc';
-import getAllRelatedVehicles from '@salesforce/apex/AccountController.getAllRelatedVehicles'
+import { LightningElement, api } from 'lwc';
+
 export default class VehicleCardList extends LightningElement {
-
-    @track selectedVehicles;
-
-    @wire(getAllRelatedVehicles, {showroomId: '$showroomOfferId'})
-    vehicles;
-
-    onVehicleSelection(event) {
-        console.log(event.target.value)
-        // this.selectedVehicles = event.detail.selectedRows;
-        this.showroomId = event.target.value;
-    }
+    @api vehicle;
 }
