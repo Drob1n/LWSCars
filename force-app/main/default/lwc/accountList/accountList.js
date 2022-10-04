cebc4a6fd2f8da641e3233d871b7aae3eb249ded
Nikoletta <nicole.snisar@gmail.com>
2022-10-04 11:47:22 +0200

    Last update 04.10

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -1,42 +1,52 @@
-import { LightningElement, wire, track} from 'lwc';
+import { LightningElement,  track} from 'lwc';
 import getAllAccounts from '@salesforce/apex/AccountController.getAllAccounts';
 import getRelatedShowrooms from '@salesforce/apex/AccountController.getRelatedShowrooms';
-import getAllRelatedVehicles from '@salesforce/apex/VehicleController.getAllRelatedVehicles'
+import getAllVehicles from '@salesforce/apex/VehicleController.getAllVehicles'
 const COLUMNS = [{label: 'Name', fieldName: 'Name'}];
 export default class AccountList extends LightningElement {
+        accounts;
+        showrooms;
+        vehicles;
         @track accountId = '';
         @track showroomId;
         @track columns = COLUMNS;
         @track vehicles;
 
-        @wire(getAllAccounts)
-        accounts;
+        connectedCallback(){
 
-        @wire(getRelatedShowrooms, {accountId: '$accountId'})
-        showrooms;
+            getAllAccounts({})
+            .then(result=>{
+                this.accounts = result;
 
+            });
+
+        }
 
         onValueSelection(event) {
-            
+
             this.accountId = event.target.value;
-            console.log(this.showrooms)
+            getRelatedShowrooms({accountId:this.accountId})
+            .then(result=>{
+                this.showrooms = result;
+
+            });
+            console.log(this.showrooms);
+
         }
+
         onValueSelection2(event) {
-
             this.showroomId = event.target.value;
-
-            getAllRelatedVehicles({showroomId: this.showroomId})
-
-            .then(result=>{this.vehicles=result;
-
-                console.log(this.vehicles);})
+            getAllVehicles({showroomId: this.showroomId})
+            .then(result=>{
+                this.vehicles=result;
+            });
+          console.log(this.vehicles);
 
         }
-
-
-
 
 }
+
+
 
 
 
--- force-app/main/default/lwc/vehicleList/vehicleList.js
+++ force-app/main/default/lwc/vehicleList/vehicleList.js
@@ -1,5 +1,56 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import  interestedChange from '@salesforce/apex/VehicleController.interestedChange';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { NavigationMixin } from 'lightning/navigation';
+// I made message text in salesforce field "Custom Labels"
+import vehiclelike from '@salesforce/label/c.vehiclelike';
 
-export default class VehicleCardList extends LightningElement {
+export default class VehicleList extends NavigationMixin(LightningElement) {
     @api vehicle;
-}
+
+    @track vehicles;
+    @track vehicleId
+    @track likeState = false;
+    label = {      
+    };
+
+
+	handleLikeButtonClick(event) {
+        const showSuccess = new ShowToastEvent({
+            message: vehiclelike,
+            variant: 'Success',
+        });
+        let vehicleId = this.vehicle.Id;
+        interestedChange ({vehicles:vehicleId})
+        .then(result=>{
+            if(this.likeState === false){
+                this.likeState = true;
+                this.dispatchEvent(showSuccess);
+            }
+            else if (this.likeState === true) {
+                this.likeState = false;
+            }
+            this.vehicles.update();
+        });
+    }
+
+    handleDetailButtonClick(event){
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: this.vehicle.Id,
+                objectApiName: 'Vehicle__c',
+                actionName: 'view'
+            }
+        });
+    }
+    
+    connectedCallback(){
+        this.likeState = this.vehicle.Interested__c;
+        
+    }
+	}
+
+	
+
+	
